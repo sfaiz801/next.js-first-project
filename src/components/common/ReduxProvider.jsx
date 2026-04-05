8ede055239ca23ff6eb336de176378e71baed3f7
sfaiz801 <sfaiz777@rediffemail.com>
2026-04-05 20:01:27 +0530

    Clean main branch: remove all project code - use development branch for active development

--- src/components/common/ReduxProvider.jsx
+++
@@ -1,8 +0,0 @@
-'use client';
-// src/components/common/ReduxProvider.jsx
-import { Provider } from 'react-redux';
-import { store } from '@/store';
-
-export default function ReduxProvider({ children }) {
-  return <Provider store={store}>{children}</Provider>;
-}
